38fa3891455e01e940bdbb3cb6eb56d601c4bd41
Vasile Gorcinschi <vasigorc@gmail.com>
2020-03-29 15:09:12 -0400

    ch02: Exercises 2.3 through 2.5

--- ch02/src/main/scala/ca/vgorcinschi/chapter02_gettingstarted/package.scala
+++ ch02/src/main/scala/ca/vgorcinschi/chapter02_gettingstarted/package.scala
@@ -18,4 +18,15 @@ package object chapter02_gettingstarted {
 
     loop(as.head, as.tail)
   }
+
+  // Exercise 2.3 Let's look at another example, currying, which converts a function f of two arguments into
+  // a function of one argument that partially applies f.
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = a => b => f(a, b)
+
+  // Exercise 2.4 Implement uncurry, which reverses the transformation of curry. Note that since =>
+  // associates to the right, A => (B => C) can be written as A => B => C
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
+
+  // Exercise 2.5 Implement the higher-order function that composes two functions
+  def compose[A,B,C](f: B => C, g: A => B): A => C = a => f(g(a))
 }
