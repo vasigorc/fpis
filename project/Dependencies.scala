a6351aa5f4b7643a5adf42f965132561bda15c04
Vasile Gorcinschi <vasigorc@gmail.com>
2022-11-19 15:14:38 -0500

    :construction: Exercise 3.27 Tree.depth

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,6 +5,7 @@ object Dependencies {
   val scalaTestVersion = "3.2.14"
   val scalaCheckVersion = "1.17.0"
   val commonDependencies: Seq[ModuleID] = Seq(
+    "org.typelevel" %% "cats-core" % "2.8.0",
     "org.scalactic" %% "scalactic" % scalaTestVersion,
     "org.scalatest" %% "scalatest" % scalaTestVersion % "test",
     "org.scalacheck" %% "scalacheck" % scalaCheckVersion % "test"
