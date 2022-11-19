8a0bf27b1d1c0ce0ec5234c5c394e96b0f77a857
Vasile Gorcinschi <vasigorc@gmail.com>
2022-11-19 15:14:38 -0500

    :construction: Exercise 3.27 Tree.depth

--- ch03/src/main/scala/ca/vgorcinschi/chapter03_datastructures/trees/Tree.scala
+++ ch03/src/main/scala/ca/vgorcinschi/chapter03_datastructures/trees/Tree.scala
@@ -1,5 +1,7 @@
 package ca.vgorcinschi.chapter03_datastructures.trees
 
+import cats.Eval
+
 sealed trait Tree[+A] {
 
   /**
@@ -7,9 +9,16 @@ sealed trait Tree[+A] {
     * @return
     */
   def size: Int
+
+  /**
+    * Exercise 3.27 Write a function depth that returns the maximum path length from the root of a tree to any leaf.
+    */
+  def depth: Int
 }
 case class Leaf[A](value: A) extends Tree[A] {
   override val size: Int = 1
+
+  override def depth: Int = 1
 }
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A] {
   override def size: Int = {
@@ -18,5 +27,28 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A] {
       case Branch(l, r)   => l.size + r.size
     }
     oneBranchSize(left) + oneBranchSize(right)
+  }
+
+  override def depth: Int = {
+
+    /**
+      * Helper method that relies on [[Eval.defer]] to avoid [[StackOverflowError]] by creating a chain of function
+      * objects on the heap (memory) - we are thus limited by the "size of the heap rather than the stack" (from
+      * [[https://www.scalawithcats.com/dist/scala-with-cats.html Scala with cats]])
+      * @param branch
+      *   branch to evaluate
+      * @return
+      *   [[Eval]] instance containing the longest depth of a branch
+      */
+    def depthHelper(branch: Tree[A]): Eval[Int] = branch match {
+      case Leaf(_) => Eval.One
+      case Branch(left, right) =>
+        for {
+          resultLeft <- Eval.defer(depthHelper(left).map(_ + 1))
+          resultRight <- Eval.defer(depthHelper(right).map(_ + 1))
+        } yield math.max(resultRight, resultLeft)
+    }
+
+    depthHelper(this).value
   }
 }
